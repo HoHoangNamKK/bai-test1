e20d4bccd7abce4b80b179450a1955c22a32e400
nam <nambolo123vn@gmail.com>
2024-08-23 13:40:08 +0700

    hieu ung nut men

---
+++ javascrip/js-main.js
@@ -0,0 +1,12 @@
+function toggleIcon_collapse(element) {
+    const icon = element.querySelector('i');
+    icon.classList.toggle('bi-list');
+    icon.classList.toggle('bi-x');
+}
+
+
+function toggleIcon_collapse_1(element) {
+    const icon = element.querySelector('i');
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+}
