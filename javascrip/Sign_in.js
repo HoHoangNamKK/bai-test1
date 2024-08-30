d0882de9cf56e8603d66e207852cf8ea460d9e80
nam <nambolo123vn@gmail.com>
2024-08-30 22:14:23 +0700

    cap nhap js cho login

--- javascrip/Sign_in.js
+++ javascrip/Sign_in.js
@@ -8,3 +8,20 @@ function hiddenPassword(Btn_hidden){
     pass.setAttribute('type',pass_type);
     console.log(pass_type);
 }
+
+
+function Checkinvalid(value_input){
+    // console.log(value_input);
+    value_input.value = value_input.value.trim();
+    if(value_input.value.trim() === ""){
+        value_input.parentElement.classList.add("invalid-tooltip");
+        value_input.parentElement.parentElement.getElementsByClassName("mess-error")[0].classList.remove("d-none");
+    }
+}
+
+function resetinvalid(value_input){
+    // console.log(value_input);
+    value_input.parentElement.classList.remove("invalid-tooltip");
+    value_input.parentElement.parentElement.getElementsByClassName("mess-error")[0].classList.add("d-none");
+    // console.log(mess_error);
+}
